0799a296c92acbc7cca6783e37ea24bcf1b42356
Chris <chansonart84@gmail.com>
2020-11-11 21:32:15 -0600

    Updated JS

--- javascript-validation-master/validation.js
+++ javascript-validation-master/validation.js
@@ -1,11 +1,25 @@
 $(document).ready(function () {
     // Object containing the validation rules
     var myRules =
-        {};
+        {
+            firstName{
+                required:true
+            }
+            grade{
+                required: true
+                min:5
+                max:8
+            }
+        };
 
     // Object containing the error messages
     var myMessages =
-        {};
+        {
+            firstName:
+            required:"First Name is Required"
+    grade:
+    required:"Grade is Required"
+        };
 
     // Pass the configuration to the form's validate() method
     // Needs submitHandler, rules, and messages properties
